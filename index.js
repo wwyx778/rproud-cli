4def5323dd4c1b1095a08c6faa4b75bccb099d90
Jehu <z_zhihao@foxmail.com>
2021-06-16 17:10:22 +0800

    feat: support webpack

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ const lang = {
   JAVASCRIPT: 'js',
   TYPESCRIPT: 'tsx',
 }[language];
+
+const ts = language === 'TYPESCRIPT';
+
 // * 创建项目文件、目录
 fs.mkdirSync(`${packageName}`);
+fs.mkdirSync(`${packageName}/public`);
+fs.mkdirSync(`${packageName}/src`);
 
-fs.writeFileSync(`${packageName}/index.html`, createFile('index.html'));
-fs.writeFileSync(`${packageName}/index.${lang}`, createFile(`${lang}/index`));
-fs.writeFileSync(`${packageName}/component.${lang}`, createFile(`${lang}/component`));
-fs.writeFileSync(`${packageName}/styles.css`, createFile(`styles`));
-fs.writeFileSync(
-  `${packageName}/package.json`,
-  createFile('package', { ts: language === 'TYPESCRIPT' }),
-);
+fs.writeFileSync(`${packageName}/public/index.html`, createFile('index.html'));
+fs.writeFileSync(`${packageName}/src/index.${lang}`, createFile(`${lang}/index`));
+fs.writeFileSync(`${packageName}/src/component.${lang}`, createFile(`${lang}/component`));
+fs.writeFileSync(`${packageName}/src/styles.css`, createFile(`styles`));
+fs.writeFileSync(`${packageName}/package.json`, createFile('package', { ts: ts }));
 
-// fs.writeFileSync(`${packageName}/webpack.config`, createFile('webpack.config.js'));
+fs.writeFileSync(`${packageName}/webpack.config.js`, createFile('webpack.config', { ts: ts }));
 
-// fs.writeFileSync(`${packageName}/postcss.config`, '');
+fs.writeFileSync(`${packageName}/postcss.config.js`, createFile('postcss.config'));
 
 fs.writeFileSync(`${packageName}/.gitignore`, 'node_modules/');
--- test.js
+++
@@ -1,3 +0,0 @@
-const path = require('path')
-console.log('__dirname：', __dirname)
-console.log('__filename：', __filename)
